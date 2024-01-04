29a0727d2d0805865391bbc29904a95b73db29a2
Gustavo <gustavo1046@hotmail.com>
2024-01-04 17:16:33 -0300

    last commit

--- src/app/Components/form/form.component.ts
+++ src/app/Components/form/form.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+  #fb = inject(FormBuilder)
 
+  public SignForm = this.#fb.group({
+    name: [''],
+    email: [''],
+    birth: [],
+    password: ['']
+  })
+  
+  public Submit(){
+    if(this.SignForm.valid){
+      console.log(this.SignForm.value);
+    } 
+  }
 }
+
--- src/app/Pages/sign-in/sign-in.component.ts
+++ src/app/Pages/sign-in/sign-in.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormComponent } from '../../Components/form/form.component';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
